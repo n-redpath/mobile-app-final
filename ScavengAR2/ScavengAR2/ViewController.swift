939fad1a5f765e4bcf00ec02c45590a15c172e9c
benmandl <54552083+benmandl@users.noreply.github.com>
2020-12-09 17:19:20 -0500

    Add files via upload

---
+++ ScavengAR2/ScavengAR2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScavengAR2
+//
+//  Created by Reid Watson on 12/9/20.
+//  Copyright © 2020 Reid Watson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
